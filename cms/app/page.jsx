c033aa71d20ae46f6a67af6770c3ec4a2d0df278
Victor <victornorlanderble@gmail.com>
2024-05-03 11:19:05 +0200

    sync dev to main

---
+++ cms/app/page.jsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
+
+const HomePage = () => {
+  return (
+    <div className="h-screen bg-slate-600">
+    <UserButton fallbackRedirectUrl="/"/>
+    </div>
+    
+  )
+}
+export default HomePage
