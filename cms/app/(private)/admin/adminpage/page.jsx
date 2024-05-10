61a1981c9754fdaecd7a434679282a0336e37e68
StiXWasHere <fabbe.nilsson123123@gmail.com>
2024-05-10 08:26:29 +0200

    changed some code

--- cms/app/(private)/admin/adminpage/page.jsx
+++ cms/app/(private)/admin/adminpage/page.jsx
@@ -5,14 +5,25 @@ import { useState } from "react"
 console.log(IsUserAdmin)
 
 const AdminPage = () => {
-  return (
+  if(IsUserAdmin) return (
+    <div>
+      <p>This user is an admin</p>
+    </div>
+  ) 
+  else return (
+    <div>
+      <p>This user is not an admin</p>
+    </div>
+  ) 
+
+  /*return (
     <div>{IsUserAdmin ? (
         <p>This user is an admin</p>
       ) : (
         <p>This user is not an admin</p>
       )}
       </div>
-  )
+  )*/
 }
 
 export default AdminPage
