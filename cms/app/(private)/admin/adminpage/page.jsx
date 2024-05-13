bf485376ac744e569adfbca9d7bec285b0cb232c
StiXWasHere <fabbe.nilsson123123@gmail.com>
2024-05-13 20:01:17 +0200

    m

--- cms/app/(private)/admin/adminpage/page.jsx
+++ cms/app/(private)/admin/adminpage/page.jsx
@@ -1,20 +1,41 @@
 "use client"
 import {IsUserAdmin} from "@/perms/adminPerms"
 import { useState } from "react"
+import { useUser } from "@clerk/nextjs";
+import { Protect } from "@clerk/nextjs";
 
-console.log(IsUserAdmin)
 
 const AdminPage = () => {
-  if(IsUserAdmin) return (
+
+  const { isSignedIn, user, isLoaded } = useUser();
+  console.log(user)
+
+  return (
+    <Protect
+      permission="org:sys_memberships:manage"
+      fallback={<p>Only an admin can access this content.</p>}
+    >
+      <div>
+        <p>Welcome</p>
+      </div>
+    </Protect>
+  )
+
+  /*if(IsUserAdmin && user) return (
     <div>
-      <p>This user is an admin</p>
+      <p>Hello admin {user.fullName}</p>
+    </div>
+  ) 
+  else if (user) return (
+    <div>
+      <p>{user.fullName}, you are not an admin</p>
     </div>
   ) 
   else return (
     <div>
-      <p>This user is not an admin</p>
+      <p>User is not logged in</p>
     </div>
-  ) 
+  )*/
 
   /*return (
     <div>{IsUserAdmin ? (
--- cms/perms/adminPerms.jsx
+++ cms/perms/adminPerms.jsx
@@ -1,5 +1,12 @@
 import { useState, useContext, createContext } from "react"
+import { useAuth } from "@clerk/nextjs"
 
 export const IsUserAdmin = () => {
-    return true
+
+    const { has } = useAuth();
+
+    const canManageSettings = has({ permission: "org:sys_memberships:manage" });
+
+    if(!canManageSettings) return false
+    else return true
 }
