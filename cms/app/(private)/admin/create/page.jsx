a82c055f9a547503e2ed70a199187b4998beb35c
StiXWasHere <fabbe.nilsson123123@gmail.com>
2024-05-30 08:18:44 +0200

    Added admin protection to routes

--- cms/app/(private)/admin/create/page.jsx
+++ cms/app/(private)/admin/create/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { CreateForm } from '../../_components/CreateForm'
+import {ServerAdminCheck} from '@/app/perms/ServerAdminCheck'
 
 const CreatePage = () => {
+  ServerAdminCheck()
   return (
     <div className='flex justify-center items-center bg-slate-500'>
         <CreateForm/>
